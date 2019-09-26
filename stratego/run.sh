347587174e1d80aacbe5cefd50202b4b3b6d239d
Rahul Sondhi <hungryhul@gmail.com>
2019-09-25 21:09:36 -0400

    Added Mongo to run shell

--- stratego/run.sh
+++ stratego/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-pkgs='maven tree'
+pkgs='maven tree mongodb'
 if !  dpkg -s $pkgs >/dev/null 2>&1; then
+  sudo apt update
   sudo apt-get install $pkgs
 fi
 (cd /tmp && git clone --depth 1 --config core.autocrlf=false https://github.com/twolfson/sexy-bash-prompt && cd sexy-bash-prompt && make install) && source ~/.bashrc
