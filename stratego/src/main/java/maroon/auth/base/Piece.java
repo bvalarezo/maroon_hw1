bec74163cf8aaec51f37f69bc2db70b51d09240d
Bryan <bryan.valarezo24@gmail.com>
2019-09-27 03:13:08 -0400

    Finalized the Piece Class
    Piece class now has setters and getters

--- stratego/src/main/java/maroon/auth/base/Piece.java
+++ stratego/src/main/java/maroon/auth/base/Piece.java
@@ -8,4 +8,50 @@ public class Piece {
     int x;
     int y;
 
+    public Piece(String id, int value){
+        this.id = id;
+        this.value = value;
+    }
+    public boolean getPlaced(){
+        return placed;
+    }
+
+    public void setPlaced(){
+        this.placed = true;
+    }
+
+    public boolean getLost(){
+        return lost;
+    }
+
+    public void setLost(){
+        this.lost = true;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getValue(){
+        return value;
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public void setX(int x){
+        this.x = x;
+    }
+
+    public int getY(){
+        return y;
+    }
+
+    public void setY(int y){
+        this.y = y;
+    }
+
+
+
 }
