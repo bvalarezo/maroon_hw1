0ae9c03dd203b6b12ba7da25ccb92a009964baad
Bryan <bryan.valarezo24@gmail.com>
2019-09-28 23:17:44 -0400

    Register.html is complete with full error response
    Fixed bugs and added implementation to respond to errors in the register page

--- stratego/src/main/java/maroon/auth/base/User.java
+++ stratego/src/main/java/maroon/auth/base/User.java
@@ -3,6 +3,7 @@ package maroon.auth.base;
 import java.util.ArrayList;
 import java.util.Set;
 import org.springframework.data.annotation.Id;
+
 import org.springframework.data.mongodb.core.index.IndexDirection;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.DBRef;
