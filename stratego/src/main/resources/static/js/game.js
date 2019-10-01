595fabd3553260905c718c4a17c795b582d3d12c
Manny <Immanuel.Almosara@stonybrook.edu>
2019-09-30 23:07:35 -0400

    uncommented my for loop code

--- stratego/src/main/resources/static/js/game.js
+++ stratego/src/main/resources/static/js/game.js
@@ -452,10 +452,10 @@ function playerTurn(game) {
             });
 
             // Ai Places Piece Here using placePiece(1, getPieceIndex(game, dragId), game, X, Y
-            // for (var i = 0; i < boardValues.length; i++) {
-            //     index = correlateValues(i, game);
-            //     placePiece(2, index, game, i % 10, Math.floor(i / 10));
-            // }
+             for (var i = 0; i < boardValues.length; i++) {
+                 index = correlateValues(i, game);
+                 placePiece(2, index, game, i % 10, Math.floor(i / 10));
+             }
         }
 
         $(".boardPlace").each(function() {
